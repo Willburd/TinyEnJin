a4a1d68ed0181eb8cf60ab8960684a170e562d23
Willburd <7099514+Willburd@users.noreply.github.com>
2025-07-03 03:25:23 -0400

    fixed duped configs

--- Template/webpack.config.js
+++ Template/webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  devtool: "source-map",
-  entry: "./Build/Tsc/Template/game.js",
-  output: {
-    path: path.resolve(__dirname, "Build"),
-    filename: "game.js",
-  },
-  optimization: {
-    minimize: false,
-  },
+	mode: "development",
+	devtool: "source-map",
+	entry: "./Build/Tsc/Template/game.js",
+	output: {
+		path: path.resolve(__dirname, "Build"),
+		filename: "game.js",
+	},
+	optimization: {
+		minimize: false,
+	},
 };
